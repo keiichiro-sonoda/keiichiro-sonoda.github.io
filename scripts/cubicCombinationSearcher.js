46f610cfb1996291f753d3de0da91515bcf94e02
Keiichiro Sonoda <keison8864@gmail.com>
2023-09-09 23:54:36 +0900

    表にした

--- scripts/cubicCombinationSearcher.js
+++ scripts/cubicCombinationSearcher.js
@@ -1,5 +1,7 @@
 async function findSolutions() {
     let maxSum = 0;
+    let triedCombinations = 0;
+    let startTime = Date.now();
 
     const targets = Array.from({ length: 100 }, (_, i) => i + 1);
 
@@ -10,13 +12,17 @@ async function findSolutions() {
                 const y = (j % 2 === 0) ? j / 2 : -(j + 1) / 2;
 
                 const k = maxSum - i - j;
-                console.log(i, j, k);
-                await new Promise(resolve => setTimeout(resolve, 10)); // To avoid blocking the UI
+                await new Promise(resolve => setTimeout(resolve, 0)); // To avoid blocking the UI
 
                 const z = (k % 2 === 0) ? k / 2 : -(k + 1) / 2;
                 const sumCubes = x ** 3 + y ** 3 + z ** 3;
+                ++triedCombinations;
+
+                console.log(`Trying ${x}^3 + ${y}^3 + ${z}^3 = ${sumCubes}`)
+
                 if (targets.includes(sumCubes)) {
-                    addToOutput(sumCubes, x, y, z);
+                    const elapsedTime = (Date.now() - startTime) / 1000; // in seconds
+                    addToOutput(sumCubes, x, y, z, elapsedTime, triedCombinations);
                     targets.splice(targets.indexOf(sumCubes), 1);
                 }
             }
@@ -26,9 +32,19 @@ async function findSolutions() {
     console.log(`No solution for ${targets}`);
 }
 
-function addToOutput(target, x, y, z) {
-    const output = document.getElementById("output");
-    const listItem = document.createElement("li");
-    listItem.innerText = `${target} = ${x}^3 + ${y}^3 + ${z}^3`;
-    output.appendChild(listItem);
+function addToOutput(target, x, y, z, elapsedTime, triedCombinations) {
+    const output = document.getElementById("output").querySelector("tbody");
+    // const listItem = document.createElement("li");
+    // listItem.innerText = `${target} = ${x}^3 + ${y}^3 + ${z}^3 | Time taken: ${elapsedTime.toFixed(2)}s | Combination tried: ${triedCombinations}`;
+
+    const row = output.insertRow();
+
+    const forumlaCell = row.insertCell(0);
+    forumlaCell.innerText = `${target} = ${x}^3 + ${y}^3 + ${z}^3`;
+
+    const timeCell = row.insertCell(1);
+    timeCell.innerText = elapsedTime.toFixed(2);
+
+    const combinationCell = row.insertCell(2);
+    combinationCell.innerText = triedCombinations;
 }
