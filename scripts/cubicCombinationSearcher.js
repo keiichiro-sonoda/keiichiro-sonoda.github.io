b8fcad3f7a7a2e6b59e8d11ca7f9c94f80f20381
Keiichiro Sonoda <keison8864@gmail.com>
2023-09-09 23:22:27 +0900

    変な説明を追加

--- scripts/cubicCombinationSearcher.js
+++ scripts/cubicCombinationSearcher.js
@@ -1,12 +1,13 @@
-function findSolutions() {
-    const maxLoopCount = 100;
+async function findSolutions() {
+    const maxLoopCount = 10000;
 
-    const targets = Array.from({length: 100}, (_, i) => i + 1);
+    const targets = Array.from({ length: 100 }, (_, i) => i + 1);
 
     for (let i = 0; i <= maxLoopCount; ++i) {
         const x = (i % 2 === 0) ? i / 2 : -(i + 1) / 2;
         for (let j = 0; j <= maxLoopCount; ++j) {
             const y = (j % 2 === 0) ? j / 2 : -(j + 1) / 2;
+            await new Promise(resolve => setTimeout(resolve, 0)); // To avoid blocking the UI
             for (let k = 0; k <= maxLoopCount; ++k) {
                 const z = (k % 2 === 0) ? k / 2 : -(k + 1) / 2;
                 const sumCubes = x ** 3 + y ** 3 + z ** 3;
